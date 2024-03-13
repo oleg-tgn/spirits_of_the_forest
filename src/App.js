7766c7ac0a035437c3e2b5cf20635f94d5d2ef95
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2024-03-14 01:19:32 +0400

    Fix Link mistake

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './Menu/Menu.js';
 import Game from './Game/Game.js';
 import Profile from './Profile/Profile.js'; 
