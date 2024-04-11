43fc481f4c075c8ba743a13d98e3457141210abd
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2024-04-11 20:05:39 +0400

    Add Tree image

--- src/Game/Tree.js
+++ src/Game/Tree.js
@@ -1,32 +1,51 @@
-import React from 'react';
-import { Rect } from 'react-konva';
+import React, { useState, useEffect } from 'react';
+import { Image } from 'react-konva';
+import useImage from 'use-image';
+import tree1Image from '../images/tree1.png'; // Обновите пути в соответствии со структурой вашего проекта
+import tree2Image from '../images/tree2.png';
+
+const TreeImage = ({ imageSrc, x, y, height }) => {
+    const [image] = useImage(imageSrc);
+    const [imgHeight, setImgHeight] = useState(height);
+    const [imgWidth, setImgWidth] = useState(0);
+
+    useEffect(() => {
+        if (image) {
+            // Подгоняем ширину изображения, сохраняя его пропорции
+            const ratio = image.width / image.height;
+            setImgWidth(imgHeight * ratio);
+        }
+    }, [image, imgHeight]);
+
+    return (
+        <Image
+            image={image}
+            x={x - imgWidth / 2}
+            y={y}
+            width={imgWidth}
+            height={imgHeight}
+        />
+    );
+};
 
 const Tree = ({ tree, x, y, hexagonHeight }) => {
-
-
     if (!tree || tree.size === 0) {
-        return null; // Не отображать дерево, если его размер равен 0
+        return null;
     }
 
-    console.log(tree)
+    const treeWidth = hexagonHeight * 0.4;
+    const treeHeight = hexagonHeight * 0.25 * tree.size;
+    const imageSrc = tree.player === 'player1' ? tree1Image : tree2Image; // Используйте импортированные изображения
 
-    
-    const treeWidth = window.innerHeight / 40;
-    const treeHeight = tree.size * 16; // Высота зависит от размера дерева
-    const fillColor = tree.player === 'player1' ? "green" : "brown"; // Зеленый для маленького, коричневый для средних и больших
+    const bottomOfHex = y + hexagonHeight / 2;
+    const adjustedY = bottomOfHex - treeHeight - hexagonHeight * 0.2;
 
-    const bottomOfHex = y + hexagonHeight / 2; // Находим нижнюю часть гексагона
-    const adjustedY = bottomOfHex - treeHeight - hexagonHeight * 0.2; // Поднимаем дерево на 20% высоты гексагона от его низа
-
-    return (        
-        <Rect
-            x={x - treeWidth / 2}
+    return (
+        <TreeImage
+            imageSrc={imageSrc}
+            x={x}
             y={adjustedY}
-            width={treeWidth}
             height={treeHeight}
-            fill={fillColor}
-            stroke="black"
-            strokeWidth={1}
         />
     );
 };
