5a3e2d06e2a1f82d0ad01a0694811f23ae7a0fe8
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2024-05-12 19:42:08 +0400

    Fix browser routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Menu from './Menu/Menu.js';
 import Game from './Game/Game.js';
 import Profile from './Profile/Profile.js'; 
@@ -10,15 +10,13 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Menu />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
