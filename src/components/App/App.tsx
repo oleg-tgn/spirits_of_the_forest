474ec08073daee1f0e0fbfbdfe3ad3a8366cf80f
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2024-05-27 15:28:52 +0400

    Try change pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
-import { Menu } from 'pages/Menu';
-import { Game } from 'pages/Game';
-import { Profile } from 'pages/Profile';
-import { Settings } from 'pages/Settings';
-import { About } from 'pages/About';
+// import { Menu } from 'pages/Menu';
+// import { Game } from 'pages/Game';
+// import { Profile } from 'pages/Profile';
+// import { Settings } from 'pages/Settings';
+// import { About } from 'pages/About';
+
+import { Menu } from '../../pages/Menu';
+import { Game } from '../../pages/Game';
+
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Menu />} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/profile" element={<Profile />} />
+       <Route path="/game" element={<Game />} />
+      {/*<Route path="/profile" element={<Profile />} />
       <Route path="/settings" element={<Settings />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />} /> */}
     </Routes>
   );
 }
