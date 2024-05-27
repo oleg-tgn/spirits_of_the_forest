58f5a3b122f1c53cb1d1fa0486d8937976a4ccec
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2024-05-27 15:16:51 +0400

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      components: '/src/components',
-      constants: '/src/constants',
-      hooks: '/src/hooks',
-      pages: '/src/pages',
-      types: '/src/types',
+      components: path.resolve(__dirname, './src/components'),
+      constants: path.resolve(__dirname, './src/constants'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      pages: path.resolve(__dirname, './src/pages'),
+      types: path.resolve(__dirname, './src/types'),
     }
   }
 })
